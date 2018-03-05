e661b1ff6f922987109e49fa3ae1b6b8f00b7d10
Aziz Kosber <a.kosber@gmail.com>
2018-03-05 17:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  if (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
 
-  if (string === "I love you, Grandma.") {
+  else (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
